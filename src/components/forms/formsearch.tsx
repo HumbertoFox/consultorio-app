ce64cda8d75e409447a69d6ff5298ee47e16ba93
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-19 12:29:07 -0300

    update validando cpf search

--- src/components/forms/formsearch.tsx
+++ src/components/forms/formsearch.tsx
@@ -12,8 +12,36 @@ interface Typesearch {
 };
 
 export default function SearchForm({ type }: Typesearch) {
-    const { register, handleSubmit, formState: { errors } } = useForm<Inputs>();
+    const { register, handleSubmit, setError, formState: { errors } } = useForm<Inputs>();
+    const getCheckedCpf = (data: string) => {
+        const isRepeatedCPF = (cpf: string) => {
+            const firstDigit = cpf[0];
+            return cpf.split('').every(digit => digit === firstDigit);
+        };
+        if (isRepeatedCPF(data)) {
+            return;
+        };
+        const calculateCheckDigit = (input: string) => {
+            let sum = 0;
+            for (let i = 0; i < input.length; i++) {
+                const digit = input.charAt(i);
+                const weight = (input.length + 1 - i);
+                sum += Number(digit) * weight;
+            };
+            const remainder = sum % 11;
+            return remainder < 2 ? '0' : (11 - remainder);
+        };
+        let primaryCheckDigit = calculateCheckDigit(data.substring(0, 9));
+        let secondaryCheckDigit = calculateCheckDigit(data.substring(0, 9) + primaryCheckDigit);
+        let correctCpf = data.substring(0, 9) + primaryCheckDigit + secondaryCheckDigit;
+        return data === correctCpf;
+    };
     const onSubmit: SubmitHandler<Inputs> = async (data) => {
+        const cpf = data.searchpatient;
+        if (!getCheckedCpf(cpf)) {
+            setError('searchpatient', { type: 'focus' }, { shouldFocus: true });
+            return;
+        };
         try {
             const formData = new FormData();
             formData.append('cpf', data.searchpatient);
