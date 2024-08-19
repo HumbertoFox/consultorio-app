39a55969615cca2e48fb94c5d0de7957dcb17c25
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-19 02:12:28 -0300

    update login menu logout delete

--- src/app/api/logout/route.ts
+++
@@ -1,9 +0,0 @@
-// 'use server';
-// import AuthService from '@/services/authservice';
-// import { NextRequest, NextResponse } from 'next/server';
-
-// export function GET(req: NextRequest) {
-//     AuthService.destroySession();
-
-//     return NextResponse.redirect(new URL('/login', req.url));
-// };
