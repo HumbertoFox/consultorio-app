07c3898ff224c4a9b81bde97297fae3782730c55
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-27 15:17:17 -0300

    update add cookies

--- src/services/removecookies.ts
+++ src/services/removecookies.ts
@@ -2,10 +2,5 @@
 import { cookies } from 'next/headers';
 
 export async function destroySession() {
-    cookies().set('session', '', {
-        expires: new Date(0),
-        path: '/',
-        domain: 'consultorio-app.vercel.app',
-        secure: true
-    });
+    cookies().delete('session');
 };
