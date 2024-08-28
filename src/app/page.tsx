d3668e0589d72a174650b839758b3c9e8afafaf5
Humberto Ribeiro <betofoxmal@gmail.com>
2024-08-28 15:14:06 -0300

    update add calendar

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,39 @@
+'use client';
 import SideBar from '@/components/sidebar';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import { eventsTest } from './api/events';
+import CustomToolbar from '@/components/toobar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+import 'react-big-calendar/lib/addons/dragAndDrop/styles.css';
 import styles from './page.module.css';
 
+interface CalendarEvent {
+  start: Date;
+  end: Date;
+  color: string;
+  [key: string]: any;
+}
+
+const localizer = momentLocalizer(moment);
+
 export default function CalendarPage() {
+  const styleColor = (event: CalendarEvent): { style: { backgroundColor: string } } => ({
+    style: { backgroundColor: event.color }
+  });
+
   return (
     <main className={styles.main}>
       <SideBar />
       <div className={styles.divmain}>
-
+        <Calendar
+          localizer={localizer}
+          events={eventsTest}
+          eventPropGetter={styleColor}
+          startAccessor='start'
+          endAccessor='end'
+          components={{ toolbar: CustomToolbar }}
+        />
       </div>
     </main>
   );
