8cc2411eb1728776278b2f644f02b7484232001b
Humberto Ribeiro <betofoxmal@gmail.com>
2024-09-19 05:40:31 -0300

    update ajustes logout

--- src/app/api/modules/actions/removecookies.ts
+++ src/app/api/modules/actions/removecookies.ts
@@ -1,6 +1,10 @@
 'use server';
 import { cookies } from 'next/headers';
 export async function destroySession() {
-    const Cookies = cookies().get('session')?.name;
-    Cookies ? cookies().delete('session') : cookies().set('session', 'value', { maxAge: 0 });
+    const sessionCookies = cookies().get('session');
+    if (sessionCookies) {
+        cookies().delete('session');
+    } else {
+        cookies().set('session', 'value', { maxAge: 0 });
+    };
 };
